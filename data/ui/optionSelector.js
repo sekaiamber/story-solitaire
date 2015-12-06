30bed6ee53f5db2677e44ea12715eeec771186c4
sekaiamber <a3824036@126.com>
2015-12-07 00:08:19 +0800

    [feature]. update.

--- data/ui/optionSelector.js
+++ data/ui/optionSelector.js
@@ -1,17 +1,31 @@
 /* global $ */
 define([
+    '../var/helper',
     './passageInit'
-], function(factory) {
+], function(helper, factory) {
     'use strict';
-    return function($moveTarget, isNoSelect, index, text) {
+    return function($moveTarget, status, index, text) {
+        status = status || 'menu';
         var $selector = factory(index, text);
-        $selector.addClass('editor').addClass('menu');
+        $selector.addClass('editor').addClass(status);
+        var $menu = helper.domMaker({
+            name: 'div',
+            class: ['cs-passage-menu'],
+            children: [{
+                name: 'div',
+                class: ['cs-passage-option', 'select']
+            }, {
+                name: 'div',
+                class: ['cs-passage-option', 'text']
+            }]
+        });
         var $text = $(".cs-passage-text", $selector);
         $text.attr('contentEditable', 'true');
         $text.focus(function() {
-            var p = $moveTarget.parent(); 
+            var p = $moveTarget.parent();
             p.scroll(p.height());
         });
+        $selector.append($menu);
         return $selector;
     }
 });
--- data/ui/passageInit.js
+++ data/ui/passageInit.js
@@ -1,12 +1,23 @@
 /* global $ */
-define(function() {
+define([
+    '../var/helper'
+], function(helper) {
     'use strict';
     return function(index, text) {
         index = index || '?';
         text = text || '';
-        var $passage = $('<div class="cs-passage" index="' + index + '"></div>');
-        $passage.append('<div class="cs-passage-text">' + text + '</div>');
-        $passage.append('<div class="cs-passage-index">' + index + '</div>');
+        var $passage = $(helper.domMaker('div', {
+            class: ['cs-passage'],
+            attr: { index: index }
+        }));
+        $passage.append(helper.domMaker('div', {
+            class: ['cs-passage-text'],
+            html: text
+        }));
+        $passage.append(helper.domMaker('div', {
+            class: ['cs-passage-index'],
+            html: index
+        }));
         return $passage;
     }
 });
